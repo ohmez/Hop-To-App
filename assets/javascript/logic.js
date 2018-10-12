eec28b5d3f9c3317089f8b53418fa044cd2db5b7
ohmez <kkid_92@hotmail.com>
2018-10-11 23:58:41 -0600

    starging on clicks

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -75,3 +75,11 @@ method: "GET"
   console.log(response);
   console.log(response.results[0].formatted_address);  
 });
+
+// start on click function listeners - libby you're to make these for splash and login atleast; communicate with others to make sure if you want to do more. 
+//here is an example of what we need
+$(document).on("click", "#loginBTN, function () {
+  // script to be added after on clicks working
+  console.log($(this)[0]);
+  console.log("^this was clicked");
+});// end singup function for new user populates new login content
