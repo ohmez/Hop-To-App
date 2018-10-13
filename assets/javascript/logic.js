626a99f4d5245fc532c01f640cf4588490a98a72
zevans98 <zman6221998@gmail.com>
2018-10-13 11:15:18 -0600

    added map to google map page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -102,4 +102,35 @@ method: "GET"
   });
 })
 })
+};
+
+//map for mapping page
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 41.85, lng: -87.65}
+  });
+  directionsDisplay.setMap(map);
+
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('start').addEventListener('change', onChangeHandler);
+  document.getElementById('end').addEventListener('change', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: "1036 E 4000 S, Salt Lake City, UT 84102, USA" ,
+    destination: "6732 west 8305 south , West Jordan, UT 84081, USA",
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
 }
